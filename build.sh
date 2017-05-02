5265148772e073173c2b83cf43db61de4d08851c
Jonathan Kosgei <jonathankosgei1000@gmail.com>
2017-05-02 14:21:00 +0300

    remove sudo

--- build.sh
+++ build.sh
@@ -4,13 +4,13 @@
 ###################
 
 # Login to the gitlab registry
-sudo docker login --username $USERNAME --password $PASSWORD gitlab.mytestlab.xyz:4567
+docker login --username $USERNAME --password $PASSWORD gitlab.mytestlab.xyz:4567
 
 # Build a new nginx image with the new index.html
-sudo docker build -t gitlab.mytestlab.xyz:4567/docker/images/green-nginx docker/nginx
+docker build -t gitlab.mytestlab.xyz:4567/docker/images/green-nginx docker/nginx
 
 # Push new image to the gitlab registry
-sudo docker push gitlab.mytestlab.xyz:4567/docker/images/green-nginx
+docker push gitlab.mytestlab.xyz:4567/docker/images/green-nginx
 
 # Update deployment
 kubectl apply -f kubernetes/deployments/green-nginx.yml
