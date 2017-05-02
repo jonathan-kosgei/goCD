6dbdd88772a7df034a4874d03182e1cffdbae5f8
Jonathan Kosgei <jonathankosgei1000@gmail.com>
2017-05-02 14:37:30 +0300

    fixed commands

--- build.sh
+++ build.sh
@@ -3,16 +3,14 @@
 # Blue Deployment 
 ###################
 
-echo $USER
-
 # Login to the gitlab registry
 sudo docker login --username $USERNAME --password $PASSWORD gitlab.mytestlab.xyz:4567
 
 # Build a new nginx image with the new index.html
-sudo docker build -t gitlab.mytestlab.xyz:4567/sudo docker/images/blue-nginx sudo docker/nginx
+sudo docker build -t gitlab.mytestlab.xyz:4567/docker/images/blue-nginx docker/nginx
 
 # Push new image to the gitlab registry
-sudo docker push gitlab.mytestlab.xyz:4567/sudo docker/images/blue-nginx
+sudo docker push gitlab.mytestlab.xyz:4567/docker/images/blue-nginx
 
 # Update deployment
 kubectl apply -f kubernetes/deployments/blue-nginx.yml
