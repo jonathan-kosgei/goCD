91f4a46c2824c741c41b7852b91b2339b95c4916
Jonathan Kosgei <jonathankosgei1000@gmail.com>
2017-05-02 10:46:07 +0300

    blue nginx

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+###################
+# Blue Deployment 
+###################
+
+# Login to the gitlab registry
+docker login --username $USERNAME --password $PASSWORD gitlab.mytestlab.xyz:4567
+
+# Build a new nginx image with the new index.html
+docker build -t gitlab.mytestlab.xyz:4567/docker/images/blue-nginx docker/nginx
+
+# Push new image to the gitlab registry
+docker push gitlab.mytestlab.xyz:4567/docker/images/blue-nginx
+
+# Update the image of the running blue nginx deployment
+kubectl set image deployment/blue-nginx nginx=gitlab.mytestlab.xyz:4567/docker/images/blue-nginx:latest
+
+# Update the proxy to proxy to the blue deployment
+kubectl apply -f kubernetes/services/proxy.yml
