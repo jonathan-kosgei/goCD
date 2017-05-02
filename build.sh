fe18f6c01f295e9a10f65ebcec92a3d9931151e6
Jonathan Kosgei <jonathankosgei1000@gmail.com>
2017-05-02 11:28:51 +0300

    green-nginx

--- build.sh
+++ build.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 ###################
-# Blue Deployment 
+# Green Deployment 
 ###################
 
 # Login to the gitlab registry
 docker login --username $USERNAME --password $PASSWORD gitlab.mytestlab.xyz:4567
 
 # Build a new nginx image with the new index.html
-docker build -t gitlab.mytestlab.xyz:4567/docker/images/blue-nginx docker/nginx
+docker build -t gitlab.mytestlab.xyz:4567/docker/images/green-nginx docker/nginx
 
 # Push new image to the gitlab registry
-docker push gitlab.mytestlab.xyz:4567/docker/images/blue-nginx
+docker push gitlab.mytestlab.xyz:4567/docker/images/green-nginx
 
-# Update the image of the running blue nginx deployment
-kubectl set image deployment/blue-nginx nginx=gitlab.mytestlab.xyz:4567/docker/images/blue-nginx:latest
+# Update the image of the running green nginx deployment
+kubectl set image deployment/green-nginx nginx=gitlab.mytestlab.xyz:4567/docker/images/green-nginx:latest
 
-# Update the proxy to proxy to the blue deployment
+# Update the proxy to proxy to the green deployment
 kubectl apply -f kubernetes/services/proxy.yml
