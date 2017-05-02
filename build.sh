4fc2a41403ff8c82f48b883bc26ebb216bafb8eb
Jonathan Kosgei <jonathankosgei1000@gmail.com>
2017-05-02 14:38:37 +0300

    added sudo

--- build.sh
+++ build.sh
@@ -4,13 +4,13 @@
 ###################
 
 # Login to the gitlab registry
-docker login --username $USERNAME --password $PASSWORD gitlab.mytestlab.xyz:4567
+sudo docker login --username $USERNAME --password $PASSWORD gitlab.mytestlab.xyz:4567
 
 # Build a new nginx image with the new index.html
-docker build -t gitlab.mytestlab.xyz:4567/docker/images/green-nginx docker/nginx
+sudo docker build -t gitlab.mytestlab.xyz:4567/docker/images/green-nginx docker/nginx
 
 # Push new image to the gitlab registry
-docker push gitlab.mytestlab.xyz:4567/docker/images/green-nginx
+sudo docker push gitlab.mytestlab.xyz:4567/docker/images/green-nginx
 
 # Update deployment
 kubectl apply -f kubernetes/deployments/green-nginx.yml
